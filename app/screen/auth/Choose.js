d3eda4a4a3f293381ed11a6ca977be219f72bd0a
Pannaga Bharadwaj S P <68677285+Pannaga-sp@users.noreply.github.com>
2022-06-28 11:03:16 +0530

    Add files via upload

---
+++ app/screen/auth/Choose.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Text } from "react-native-paper"
+import Background from "../../../features/Background"
+import Button from "../../../features/Button"
+import BackButton from '../../../features/BackButton';
+import UserLoginScreen from './UserLoginScreen'
+
+export default function Choose({ navigation }) {
+    return (
+        <Background>
+            <BackButton goBack={navigation.navigate('UserLoginScreen')} />
+            <Button mode="contained" onPress={() => navigation.navigate("Quiz1")} color='#F8772E'>
+                Quiz
+            </Button>
+            <Button mode="contained" onPress={() => navigation.navigate("upload")} color='#F8772E'>
+                Upload Picture
+            </Button>
+            <Button mode="contained" onPress={() => navigation.navigate("qrcode")} color='#F8772E'>
+                QR Scan
+            </Button>
+        </Background>
+
+    )
+}
